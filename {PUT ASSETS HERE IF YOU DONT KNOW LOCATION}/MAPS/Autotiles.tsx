a295b6b63489fe49cd43a0e7802350a48664d2a4
Wacko <68190565+weirdwacko@users.noreply.github.com>
2021-12-21 13:13:09 -0500

    Pecan Town + New Tilesets

---
+++ {PUT ASSETS HERE IF YOU DONT KNOW LOCATION}/MAPS/Autotiles.tsx
@@ -0,0 +1,160 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Autotiles" tilewidth="1920" tileheight="128" tilecount="52" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="32" source="../Ekat's Public Tiles v3.0/Autotiles/Candles.png"/>
+ </tile>
+ <tile id="1">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Coral_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Default_1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Default_2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Default_3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Default_4.png"/>
+ </tile>
+ <tile id="6">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Default_5.png"/>
+ </tile>
+ <tile id="7">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Default_6.png"/>
+ </tile>
+ <tile id="8">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Desert_1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Desert_2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Desert_3.png"/>
+ </tile>
+ <tile id="11">
+  <image width="384" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Fireplace.png"/>
+ </tile>
+ <tile id="12">
+  <image width="288" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Flag_1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="288" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Flag_2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="288" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Flag_3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="32" source="../Ekat's Public Tiles v3.0/Autotiles/Flower_White.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="32" source="../Ekat's Public Tiles v3.0/Autotiles/Flowers_Red.png"/>
+ </tile>
+ <tile id="17">
+  <image width="1152" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Grandfather_Clock_1.png"/>
+ </tile>
+ <tile id="18">
+  <image width="1152" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Grandfather_Clock_2.png"/>
+ </tile>
+ <tile id="19">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Harvest_1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Harvest_2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="512" height="32" source="../Ekat's Public Tiles v3.0/Autotiles/Ice_1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="640" height="32" source="../Ekat's Public Tiles v3.0/Autotiles/Ice_2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Jonquil_1.png"/>
+ </tile>
+ <tile id="24">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Jonquil_2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="768" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Jonquil_3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="256" height="32" source="../Ekat's Public Tiles v3.0/Autotiles/Jonquil_4.png"/>
+ </tile>
+ <tile id="27">
+  <image width="768" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Jonquil_5.png"/>
+ </tile>
+ <tile id="28">
+  <image width="1152" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Lantern_1.png"/>
+ </tile>
+ <tile id="29">
+  <image width="1152" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Lantern_2.png"/>
+ </tile>
+ <tile id="30">
+  <image width="384" height="32" source="../Ekat's Public Tiles v3.0/Autotiles/Lava.png"/>
+ </tile>
+ <tile id="31">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Moon_1.png"/>
+ </tile>
+ <tile id="32">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Moon_2.png"/>
+ </tile>
+ <tile id="33">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Moon_3.png"/>
+ </tile>
+ <tile id="34">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Moon_4.png"/>
+ </tile>
+ <tile id="35">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Moon_5.png"/>
+ </tile>
+ <tile id="36">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Moon_6.png"/>
+ </tile>
+ <tile id="37">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Ruins_1.png"/>
+ </tile>
+ <tile id="38">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Ruins_2.png"/>
+ </tile>
+ <tile id="39">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Ruins_3.png"/>
+ </tile>
+ <tile id="40">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Ruins_4.png"/>
+ </tile>
+ <tile id="41">
+  <image width="384" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Seaweed_1.png"/>
+ </tile>
+ <tile id="42">
+  <image width="576" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Seaweed_2.png"/>
+ </tile>
+ <tile id="43">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Snow_1.png"/>
+ </tile>
+ <tile id="44">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Snow_2.png"/>
+ </tile>
+ <tile id="45">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Snow_3.png"/>
+ </tile>
+ <tile id="46">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Snow_4.png"/>
+ </tile>
+ <tile id="47">
+  <image width="1920" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Snow_6.png"/>
+ </tile>
+ <tile id="48">
+  <image width="480" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/TV_1.png"/>
+ </tile>
+ <tile id="49">
+  <image width="192" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/TV_2.png"/>
+ </tile>
+ <tile id="50">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Volcano_1.png"/>
+ </tile>
+ <tile id="51">
+  <image width="96" height="128" source="../Ekat's Public Tiles v3.0/Autotiles/Volcano_2.png"/>
+ </tile>
+</tileset>
---
+++ {PUT ASSETS HERE IF YOU DONT KNOW LOCATION}/MAPS/Outdoors Gen 3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Outdoors Gen 3" tilewidth="32" tileheight="32" tilecount="728" columns="8">
+ <image source="../0sIaKYt.png" width="256" height="2924"/>
+</tileset>
